4d8d72154a9a4ec54b6d483701e117bbb82bbd79
happsie <njesper98@gmail.com>
2023-11-18 11:50:47 +0100

    cleanup jwt config for guarded routes

--- router/jwt_config.go
+++ router/jwt_config.go
@@ -3,17 +3,11 @@ package router
 import (
 	"github.com/happise/pixelwars/container"
 	echojwt "github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
-	"slices"
 )
 
 func GetJwtConfig(container container.Container) echojwt.Config {
-	jwtWhitelist := []string{"/api/auth/login", "/api/auth/callback", "", "/", "/css*", "/images*"}
 	return echojwt.Config{
 		TokenLookup: "cookie:token",
 		SigningKey:  []byte(container.GetConfig().JWT.Secret),
-		Skipper: func(c echo.Context) bool {
-			return slices.Contains(jwtWhitelist, c.Path())
-		},
 	}
 }
